e2cc2acd7fe6ff3a4f7c0f4ca9ad62e21abaaf3b
Atanas Yonkov <atanasyonkov1@gmail.com>
2025-01-07 09:32:08 +0200

    Install and test vuetify and cal library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
+    css: [
+        'vuetify/lib/styles/main.sass',
+        '@mdi/font/css/materialdesignicons.css',
+    ],
+    build: {
+        transpile: ['vuetify'],
+    },
+    vite: {
+        define: {
+            'process.env.DEBUG': false,
+        },
+        ssr: {
+            // if ssr enabled in future, this config is required to load vuetify properly
+            noExternal: ['vuetify'],
+        },
+    },
+    modules: [
+        (options, nuxt) => {
+            nuxt.hooks.hook('vite:extendConfig', (config) =>
+                // @ts-ignore
+                config.plugins.push(
+                    vuetify({
+                        autoImport: true,
+                    }),
+                ),
+            )
+        },
+    ],
 })
---
+++ plugins/vuetify.ts
@@ -0,0 +1,10 @@
+import { createVuetify } from 'vuetify'
+
+export default defineNuxtPlugin((nuxtApp) => {
+    const vuetify = createVuetify({
+        icons: {
+            defaultSet: 'mdi',
+        },
+    })
+    nuxtApp.vueApp.use(vuetify)
+})
