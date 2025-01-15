5533692aa5c5494429d545a0ac95f4ba502edb7d
Atanas Yonkov <atanasyonkov1@gmail.com>
2025-01-15 23:26:54 +0200

    Work n account page

--- components/Account.vue
+++ components/Account.vue
@@ -5,18 +5,19 @@ const user = useSupabaseUser()
 const loading = ref(true)
 const username = ref('')
 const website = ref('')
+const fullname = ref('')
 const avatar_path = ref('')
 
 loading.value = true
 const { data } = await supabase
     .from('profiles')
-    .select(`username, website, avatar_url`)
+    .select(`username, full_name, avatar_url`)
     .eq('id', user.value.id)
     .single()
 
 if (data) {
     username.value = data.username
-    website.value = data.website
+    fullname.value = data.full_name
     avatar_path.value = data.avatar_url
 }
 loading.value = false
@@ -29,7 +30,7 @@ async function updateProfile() {
         const updates = {
             id: user.value.id,
             username: username.value,
-            website: website.value,
+            full_name: fullname.value,
             avatar_url: avatar_path.value,
             updated_at: new Date(),
         }
@@ -47,13 +48,14 @@ async function updateProfile() {
 </script>
 
 <template>
-    <div class="flex items-start justify-center mt-10">
+    <div class="mt-10 px-10 pb-8">
+        <p class="font-black text-5xl mb-6">Hi, {{ fullname.split(' ')[0] }}</p>
         <v-card
-            class="mx-auto pa-10 pb-8 w-full"
+            class="mx-auto w-full"
             elevation="0"
             max-width="448"
             rounded="lg">
-            <div class="text-subtitle-1 text-medium-emphasis">Name</div>
+            <div class="text-subtitle-1 text-medium-emphasis">Username</div>
 
             <v-text-field
                 class="mb-5"
@@ -63,27 +65,17 @@ async function updateProfile() {
                 prepend-inner-icon="mdi-account"
                 variant="outlined"></v-text-field>
 
-            <div class="text-subtitle-1 text-medium-emphasis">Email</div>
+            <div class="text-subtitle-1 text-medium-emphasis">Full Name</div>
 
             <v-text-field
                 class="mb-5"
                 density="compact"
-                v-model="user.email"
-                placeholder="Email address"
+                v-model="fullname"
+                placeholder="John Doe"
                 prepend-inner-icon="mdi-email-outline"
                 variant="outlined"></v-text-field>
 
-            <div class="text-subtitle-1 text-medium-emphasis">Website</div>
-
-            <v-text-field
-                class="mb-5"
-                density="compact"
-                v-model="website"
-                placeholder="www.website.com"
-                prepend-inner-icon="mdi-web"
-                variant="outlined"></v-text-field>
-
-            <div class="flex gap-10 justify-between mt-10">
+            <div class="flex">
                 <v-btn
                     class="flex-grow"
                     color="blue"
@@ -93,5 +85,19 @@ async function updateProfile() {
                 </v-btn>
             </div>
         </v-card>
+
+        <div class="mt-10 text-2xl font-black">History</div>
+        <ul>
+            <li class="indent-4 text-xl mt-2">
+                January (857 лв.)
+
+                <p class="indent-8 text-sm">Възрастни: 3</p>
+                <p class="indent-8 text-sm">Деца: 3</p>
+                <p class="indent-8 text-sm">Частни: 3</p>
+            </li>
+
+            <li class="indent-4 text-xl mt-2">February (581 лв.)</li>
+            <li class="indent-4 text-xl mt-2">March (1230 лв.)</li>
+        </ul>
     </div>
 </template>
